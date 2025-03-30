fcd0c0d6aadcd89ed1392ddf8258385d851e3ece
Kiversayz <juzama12@gmail.com>
2025-03-30 17:27:08 +0300

    Модуль 16 - 1 Фреймворки - Готово

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin # type: ignore
 # Register your models here.
 from users.models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'pk')
+    list_filter = ('last_name',)
